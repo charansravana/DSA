1dc70a482d5fd705f645b3abebf75c7b6df8d022
charan <charansravana2003@gmail.com>
2025-07-08 12:48:59 +0530

    array

---
+++ Array/leet_219.py
@@ -0,0 +1,23 @@
+'''
+219. Contains Duplicate II
+Given an integer array nums and an integer k, return true if there are two distinct indices i and j in the array such that nums[i] == nums[j] and abs(i - j) <= k.
+
+Example 1:
+Input: nums = [1,2,3,1], k = 3
+Output: true
+
+Example 2:
+Input: nums = [1,0,1,1], k = 1
+Output: true
+
+'''
+
+def containsNearbyDuplicate(self, nums,k) :
+        seen = set()
+        for i in range(len(nums)):
+            if nums[i] in seen:
+                return True
+            seen.add(nums[i])
+            if len(seen) > k:
+                seen.remove(nums[i - k])
+        return False
